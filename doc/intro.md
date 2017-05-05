4483bb5064959d452c0ac16d8d62b4f61a9f0271
jonathanDuenas <jonathanduenas94@gmail.com>
2017-05-05 09:40:04 -0600

    Initial commit

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to natal-wrapper
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject natal-wrapper "0.1.0-SNAPSHOT"
+  :description "A wrapper for the re-natal library"
+  :url "http://github.com/jonathanDuenas/natal-wrapper"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.9.0-alpha10"]
+                 [org.clojure/clojurescript "1.9.198"]])
